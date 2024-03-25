97258fd827d5f6ab3910c225e7039f3d5dc76c46
Goran Košutić <kosuticg76@hotmail.com>
2024-03-25 23:53:51 +0100

    djrf

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -17,11 +17,18 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from restaurant import views
+
+router = DefaultRouter()
+router.register(r"tables", views.BookingViewSet)
+router.register(r"users", views.UserViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
-        "restaurant/",
+        "restaurant/menu/",
         include("restaurant.urls"),
     ),
+    path("restaurant/booking/", include(router.urls)),
 ]
